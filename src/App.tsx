cd5a01d20f61d1a624f9424fd8bd9312266d53f7
Dan Goldman <101563504+dangoldmann@users.noreply.github.com>
2025-02-10 11:23:11 -0300

    fix: comment out Sign In button in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -63,9 +63,9 @@ function App() {
               </span>
             </div>
             <div className='flex items-center space-x-4'>
-              <button className='text-gray-600 hover:text-gray-900'>
+              {/* <button className='text-gray-600 hover:text-gray-900'>
                 Sign In
-              </button>
+              </button> */}
               <button className='bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700'>
                 Get Started
               </button>
